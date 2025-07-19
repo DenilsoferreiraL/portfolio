f1e3a25d462fc472f23c9dca01529d8104749a28
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-07-19 16:15:05 -0300

    fix: update

--- src/data/navLinks.ts
+++
@@ -1,6 +0,0 @@
-export const navLinks = [
-    { label: 'Início', href: '#home' },
-    { label: 'Habilidades', href: '#skills' },
-    { label: 'Habilidades Interpessoais', href: '#soft-skills' },
-    { label: 'Projetos', href: '#projects' },
-];
